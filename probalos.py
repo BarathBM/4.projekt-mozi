1c0f1d18ede58fc27384cc316a42211dc53aa189
OregTuz00001 <kanyaimark2007@gmail.com>
2025-03-28 08:09:13 +0100

    agyfasz

--- probalos.py
+++ probalos.py
@@ -1,12 +1,26 @@
 from tkinter import *
 import sqlite3
+import os
+from PIL import Image
+Image.CUBIC = Image.BICUBIC
+import ttkbootstrap as tb
+from ttkbootstrap.constants import *
+from tkinter import messagebox, Toplevel, Entry, Label, Button, IntVar, Spinbox
+from fpdf import FPDF
 
 
-root = Tk()
-root.title("Adatbázis létrehozása")
-root.geometry("400x400")
+root=tb.Window(themename="darkly")
+root.title("Jegyfoglalos")
+root.geometry("800x600")
 
-
+label = tb.Label(root, text="Válassz filmet:", font=("Arial", 16))
+label.pack(pady=10)
+    
+film_lista = tb.Treeview(root, columns=("terem", "cim", "helyek"), show="headings")
+film_lista.heading("terem", text="Terem")
+film_lista.heading("cim", text="Film címe")
+film_lista.heading("helyek", text="Szabad helyek")
+film_lista.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
 def Folgalolist():
     seat_window = Toplevel(root)
